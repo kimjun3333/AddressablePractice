779f7981b4030888754962a3cfec42c44bc51bcb
kimjun3333 <kheami35@gmail.com>
2025-11-07 03:28:15 +0900

    [Fix] StoreUI 살짝 변경 아직 테스트중

--- AddressablePractice/Assets/Scripts/UI/StoreUI.cs
+++ AddressablePractice/Assets/Scripts/UI/StoreUI.cs
@@ -5,22 +5,33 @@ using UnityEngine;
 public class StoreUI : UIBase
 {
     [SerializeField] private Transform contextParent;
-    [SerializeField] private GameObject cardSlotPrefab;
+    [SerializeField] private CardUI cardPrefab;
     [SerializeField] private List<CardSO> cardList;
 
     public override void OnInit()
     {
         foreach (var so in cardList)
         {
-            var slotObj = Instantiate(cardSlotPrefab, contextParent);
-            var slot = slotObj.GetComponent<CardStoreUI>();
-            slot.Setup(so);
-            slot.OnBuyRequested = HandleBuyCard;
+            //var slotObj = Instantiate(cardSlotPrefab, contextParent);
+            //var slot = slotObj.GetComponent<CardStoreUI>();
+            //slot.Setup(so);
+            //slot.OnBuyRequested = HandleBuyCard;
+
+            var data = new CardData(so);
+
+            var cardUI = Instantiate(cardPrefab, contextParent);
+            cardUI.SetUp(data, OnBuyCard);
         }
     }
 
-    private void HandleBuyCard(CardSO card)
+    private void OnBuyCard(CardData card)
     {
-        Debug.Log("카드 구매!");
+        Debug.Log($"{card.BaseSO.Name} 구매!");
+        // 구매 로직 나중에 추가 예정
     }
+
+    //private void HandleBuyCard(CardSO card)
+    //{
+    //    Debug.Log("카드 구매!");
+    //}
 }
